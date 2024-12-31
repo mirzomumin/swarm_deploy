e07f02429e414df51495133a32f33a272bfa450d
Mirzomumin <mirzomumin@list.ru>
2024-12-31 12:08:51 +0300

    Get value from environment variables

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*jepiwwd=1t0mhi1vop4!gv9rwu%nahhjr_xs$gfaz_gw-36j'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
